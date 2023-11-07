e204358f7f3fe7575715b3b4969291ecb91d81f0
Martin-mendez <130391732+Martin-mendez@users.noreply.github.com>
2023-11-07 14:44:51 -0300

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import Home from './Home';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Mi Aplicación de React</h1>
+      </header>
+      <main>
+        <Home />
+      </main>
+    </div>
+  );
+}
+
+export default App;
